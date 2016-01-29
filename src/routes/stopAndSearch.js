c6820f2a6a2bc490f8707d14b7be1e35d86a5045
Alex Chesters <alex.chesters@bbc.co.uk>
2016-01-29 14:13:49 +0000

    Add stop and search route

---
+++ src/routes/stopAndSearch.js
@@ -0,0 +1,13 @@
+var makeRequest = require('../common/makeRequest');
+var PATHS = require('../common/paths');
+
+/**
+ * Make a request for street level crimes based on a location.
+ * @param {object} location - An object containing latitude and longitude values.
+ * @returns {promise}
+ */
+module.exports = function(location) {
+  const lat = location.latitude;
+  const lng = location.longitude;
+  return makeRequest(`${PATHS.STOP_AND_SEARCH}?lat=${lat}&lng=${lng}`);
+};
