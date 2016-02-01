2bef04dd67ed8068709cafe7bc89afb8a9e2dda9
Alex Chesters <alex.chesters@bbc.co.uk>
2016-02-01 17:38:13 +0000

    Fix missing leading slash

--- src/common/paths.js
+++ src/common/paths.js
@@ -1,5 +1,5 @@
 module.exports = {
   STREET_LEVEL: '/crimes-street/all-crime',
   STOP_AND_SEARCH: '/stops-force',
-  AVAILABILITY: 'crimes-street-dates'
+  AVAILABILITY: '/crimes-street-dates'
 };
