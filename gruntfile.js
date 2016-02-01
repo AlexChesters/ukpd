12dc136d263a6f422f1d141acae8cbe60dddfd1a
Alex Chesters <alex.chesters@bbc.co.uk>
2016-02-01 08:05:19 +0000

    Use .jshintrc file

--- gruntfile.js
+++ gruntfile.js
@@ -4,27 +4,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        node: true,
-        jasmine: true,
-        esnext: true,
-        globals: {
-          fdescribe: true,
-          beforeAll: true,
-          pending: true
-        }
-      },
+      options: { jshintrc: true },
       gruntfile: { src: 'gruntfile.js' },
       src: { src: 'src/**/*.js' }
     },
