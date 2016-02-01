051addf3341ad384311f001a9eced00fad942807
Alex Chesters <alex.chesters@bbc.co.uk>
2016-02-01 17:43:50 +0000

    Add date to stop and search

--- src/routes/stopAndSearch.js
+++ src/routes/stopAndSearch.js
@@ -4,8 +4,9 @@ var PATHS = require('../common/paths');
 /**
  * Make a request for stop and searches based on a force.
  * @param {string} force - An valid force ID.
+ * @param {string} date - A date in the format YYYY-MM.
  * @returns {promise}
  */
-module.exports = function(force) {
-  return makeRequest(`${PATHS.STOP_AND_SEARCH}?force=${force}`);
+module.exports = function(force, date) {
+  return makeRequest(`${PATHS.STOP_AND_SEARCH}?force=${force}&date=${date}`);
 };
