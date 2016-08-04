378e7336ada3d96765c6b6628cb737d1a00d2a7e
Alex Chesters <alex.chesters@bbc.co.uk>
2016-08-04 20:33:04 +0100

    Rename test

--- test/integration/availabilitySpec.js
+++ test/integration/availabilitySpec.js
@@ -6,14 +6,16 @@ const UKPD = require('../../src/UKPD')
 const successData = require('../../stub-data/availability/success')
 
 describe('availability', function () {
-  it('should return the stub data', function (done) {
-    // Intercept request and return the stub data
-    nock('https://data.police.uk')
-      .get('/api/crimes-street-dates')
-      .reply(200, successData)
-    UKPD
-      .availability()
-      .then((data) => { expect(data).toEqual(successData); done() })
-      .catch(() => done.fail(new Error('Promise should not be rejected')))
+  describe('when valid data is returned', function () {
+    it('should resolve with that data', function (done) {
+      // Intercept request and return the stub data
+      nock('https://data.police.uk')
+        .get('/api/crimes-street-dates')
+        .reply(200, successData)
+      UKPD
+        .availability()
+        .then((data) => { expect(data).toEqual(successData); done() })
+        .catch(() => done.fail(new Error('Promise should not be rejected')))
+    })
   })
 })
