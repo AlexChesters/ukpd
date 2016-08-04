a4fed20b1bd5ecab3ad72dd5648ba478599d4903
Alex Chesters <alex.chesters@bbc.co.uk>
2016-08-04 20:08:50 +0100

    Fix linting errors

--- src/common/makeRequest.js
+++ src/common/makeRequest.js
@@ -1,19 +1,19 @@
-var request = require('request');
+var request = require('request')
 
-const baseURL = 'https://data.police.uk/api';
+const baseURL = 'https://data.police.uk/api'
 
-module.exports = function(path) {
-  return new Promise(function(resolve, reject) {
-    request(`${baseURL}${path}`, function(error, response, body) {
-      if (error) { return reject(error); }
-      if (response.statusCode !== 200) { return reject({message: `Non-200 status code received: ${response.statusCode}`}); }
-      var data;
+module.exports = function (path) {
+  return new Promise(function (resolve, reject) {
+    request(`${baseURL}${path}`, function (error, response, body) {
+      if (error) { return reject(error) }
+      if (response.statusCode !== 200) { return reject({message: `Non-200 status code received: ${response.statusCode}`}) }
+      var data
       try {
-        data = JSON.parse(body);
+        data = JSON.parse(body)
       } catch (e) {
-        return reject(e);
+        return reject(e)
       }
-      return resolve(data);
-    });
-  });
-};
+      return resolve(data)
+    })
+  })
+}
