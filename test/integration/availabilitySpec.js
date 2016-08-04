b6f708fde248b52f61b5d8531dce614827cbec67
Alex Chesters <alex.chesters@bbc.co.uk>
2016-08-04 20:31:54 +0100

    Add availability integration test

--- test/integration/availabilitySpec.js
+++ test/integration/availabilitySpec.js
@@ -1,7 +1,19 @@
 /* eslint-env jasmine */
 
+const nock = require('nock')
+
+const UKPD = require('../../src/UKPD')
+const successData = require('../../stub-data/availability/success')
+
 describe('availability', function () {
-  it('should pass', function () {
-    expect(true).toEqual(true)
+  it('should return the stub data', function (done) {
+    // Intercept request and return the stub data
+    nock('https://data.police.uk')
+      .get('/api/crimes-street-dates')
+      .reply(200, successData)
+    UKPD
+      .availability()
+      .then((data) => { expect(data).toEqual(successData); done() })
+      .catch(() => done.fail(new Error('Promise should not be rejected')))
   })
 })
