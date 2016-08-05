1ea8e67fd0948ee2faaf933913604ec601d073ff
Alex Chesters <alex.chesters@bbc.co.uk>
2016-08-05 14:27:33 +0100

    Update describe block

--- test/spec/common/makeRequestSpec.js
+++ test/spec/common/makeRequestSpec.js
@@ -5,26 +5,28 @@ const nock = require('nock')
 const makeRequest = require('../../../src/common/makeRequest')
 
 describe('makeRequest', function () {
-  describe('when valid data is returned', function () {
-    it('should resolve with that data', function (done) {
-      // Intercept request and return the stub data
-      nock('https://data.police.uk/api')
-        .get('/foobar')
-        .reply(200, [{data: 'some-date'}])
-      makeRequest('/foobar')
-        .then((data) => { expect(data).toEqual([{data: 'some-date'}]); done() })
-        .catch(() => done.fail(new Error('Promise should not be rejected')))
+  describe('when a sucessful request is made', function () {
+    describe('and valid data is returned', function () {
+      it('should resolve with that data', function (done) {
+        // Intercept request and return the stub data
+        nock('https://data.police.uk/api')
+          .get('/foobar')
+          .reply(200, [{data: 'some-date'}])
+        makeRequest('/foobar')
+          .then((data) => { expect(data).toEqual([{data: 'some-date'}]); done() })
+          .catch(() => done.fail(new Error('Promise should not be rejected')))
+      })
     })
-  })
-  describe('when invalid data is returned', function () {
-    it('should reject with an error', function (done) {
-      // Intercept request and return the stub data
-      nock('https://data.police.uk/api')
-        .get('/foobar')
-        .reply(200, '<h1>Welcome to my site</h1>')
-      makeRequest('/foobar')
-        .then(() => done.fail(new Error('Promise should not be rejected')))
-        .catch((error) => { expect(error).toEqual('Invalid JSON'); done() })
+    describe('and invalid data is returned', function () {
+      it('should reject with an error', function (done) {
+        // Intercept request and return the stub data
+        nock('https://data.police.uk/api')
+          .get('/foobar')
+          .reply(200, '<h1>Welcome to my site</h1>')
+        makeRequest('/foobar')
+          .then(() => done.fail(new Error('Promise should not be rejected')))
+          .catch((error) => { expect(error).toEqual('Invalid JSON'); done() })
+      })
     })
   })
 })
