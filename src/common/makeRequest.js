8c4f64de061c78e10454bd69a5e3d361e8061f4b
Alex Chesters <alex.chesters@bbc.co.uk>
2016-09-19 14:30:09 +0100

    Use fetch

--- src/common/makeRequest.js
+++ src/common/makeRequest.js
@@ -1,17 +1,7 @@
 'use strict'
 
-const request = require('request')
+const fetch = require('node-fetch')
 
 module.exports = (path) => {
-  return new Promise((resolve, reject) => {
-    request(`https://data.police.uk/api${path}`, (error, response, body) => {
-      if (error) { return reject(error) }
-      if (response.statusCode !== 200) { return reject(`Non-200 status code received (${response.statusCode})`) }
-      try {
-        return resolve(JSON.parse(body))
-      } catch (e) {
-        return reject('Invalid JSON')
-      }
-    })
-  })
+  return fetch(`https://data.police.uk/api${path}`).then(res => res.json())
 }
