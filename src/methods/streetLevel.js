71ed66c0477ce81b98a48a6db4bccf1343507135
Alex Chesters <alex.chesters@bbc.co.uk>
2016-09-20 09:48:46 +0100

    Add date to street level

--- src/methods/streetLevel.js
+++ src/methods/streetLevel.js
@@ -5,6 +5,7 @@ const PATHS = require('../common/paths')
  * Make a request for street level crimes based on a location.
  * @param {number} latitude - A number representing a latitude value.
  * @param {number} longitude - A number representing a longitude value.
+ * @param {string} date - Optional. A date in the format YYYY-MM
  * @returns {promise}
  */
-module.exports = (latitude, longitude) => makeRequest(`${PATHS.STREET_LEVEL}?lat=${latitude}&lng=${longitude}`)
+module.exports = (latitude, longitude, date) => makeRequest(`${PATHS.STREET_LEVEL}?lat=${latitude}&lng=${longitude}&date=${date}`)
