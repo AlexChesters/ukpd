40e467001543a60462a769444112055c4dd9a369
Alex Chesters <alex.chesters@bbc.co.uk>
2016-11-15 13:19:32 +0000

    Call standard correctly

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,10 @@
 const gulp = require('gulp')
-const standard = require('gulp-standard')
 const jasmine = require('gulp-jasmine')
 
 gulp.task('jasmine', function () {
   return gulp
     .src('test/**/*Spec.js')
     .pipe(jasmine({verbose: true}))
-})
-
-gulp.task('lint', function () {
-  return gulp
-    .src(['gulpfile.js', 'src/**/*.js', 'test/**/*.js'])
-    .pipe(standard())
-    .pipe(standard.reporter('default', {
-      breakOnError: true
-    }))
 })
 
 gulp.task('test', ['jasmine', 'lint'])
