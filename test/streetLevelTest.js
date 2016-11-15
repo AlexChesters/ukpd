9dfe7ea20e1466caa6a112f477c1affb0670d643
Alex Chesters <alex.chesters@bbc.co.uk>
2016-11-15 13:52:42 +0000

    Add passing test

--- test/streetLevelTest.js
+++ test/streetLevelTest.js
@@ -7,8 +7,11 @@ const UKPD = require('../src/UKPD')
 
 describe('Street level', function () {
   this.timeout(5000)
-  it('should fail', function () {
+  it('should return the expected data structure', function () {
     return UKPD().streetLevel('52.629729', '-1.131592')
-      .then((data) => expect(data).to.equal(true))
+      .then((data) => {
+        expect(data).to.be.an('array')
+        expect(data.length).to.be.above(0)
+      })
   })
 })
