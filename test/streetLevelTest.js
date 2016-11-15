7f6f17a78d8b1f16589a8c303da9512dbff2b8d6
Alex Chesters <alex.chesters@bbc.co.uk>
2016-11-15 13:42:25 +0000

    Add mocha

---
+++ test/streetLevelTest.js
@@ -0,0 +1,10 @@
+/* eslint-env mocha */
+
+const chai = require('chai')
+const expect = chai.expect
+
+describe('Street level', function () {
+  it('should fail', function () {
+    expect(true).to.equal(false)
+  })
+})
