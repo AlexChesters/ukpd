c23e728ccef8c284d751520a640f1e56d7c50b56
Alex Chesters <alex.chesters@bbc.co.uk>
2016-11-18 09:27:57 +0000

    Add forces method

--- src/common/paths.js
+++ src/common/paths.js
@@ -1,4 +1,5 @@
 module.exports = {
   STREET_LEVEL: '/crimes-street/all-crime',
-  STOP_AND_SEARCH: '/stops-street'
+  STOP_AND_SEARCH: '/stops-street',
+  FORCES: '/forces'
 }
---
+++ src/methods/forces.js
@@ -0,0 +1,8 @@
+const fetchData = require('../common/fetch_data')
+const PATHS = require('../common/paths')
+
+/**
+ * Make a request for either the list of forces or a specific force.
+ * @returns {promise}
+ */
+module.exports = (force = '') => fetchData(`${PATHS.FORCE}/${force}`)
