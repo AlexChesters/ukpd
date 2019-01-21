1383df6163b3ba0c477c9c86a568704330fa3e76
Alex Chesters <alex.chesters@bbc.co.uk>
2019-01-21 16:24:41 +0000

    add types

---
+++ src/types.d.ts
@@ -0,0 +1,6 @@
+export function availability(): object[]
+export function categories(): object[]
+export function forces(force?: string): object[]
+export function lastUpdated(): object[]
+export function stopAndSearch(latitude: number, longitude: number, date?: string): object[]
+export function streetLevel(latitude: number, longitude: number, date?: string): object[]
