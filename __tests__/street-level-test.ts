a82e0fb47d60f4b113225075cf74527a1a589cd4
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2020-01-22 17:24:23 +0000

    update type

--- __tests__/street-level-test.ts
+++ __tests__/street-level-test.ts
@@ -5,13 +5,15 @@ import * as R from 'ramda'
 
 import * as UKPD from '../src'
 
+import { ICrime } from '../src/interfaces'
+
 const expect = chai.expect
 
 describe('Street level', function () {
   jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000
   test('should return the expected data structure', function () {
     return UKPD.streetLevel(52.629729, -1.131592)
-      .then((data: object[]) => {
+      .then((data: ICrime[]) => {
         expect(data).to.be.an('array')
         expect(data.length).to.be.above(0)
         R.forEach((item) => expect(item).to.be.an('object'), data)
