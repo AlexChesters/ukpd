bf9d4f7379547c5058c594ef495243d61d2001ec
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2020-07-02 10:55:18 +0100

    Revert "fix fetch data result type"
    This reverts commit 6a6e2dd3093d69e932a1abf793c79a53f7d6c69a.

--- src/utils/fetch-data.ts
+++ src/utils/fetch-data.ts
@@ -1,6 +1,7 @@
 import * as fetch from 'isomorphic-fetch'
 
-export default async (endpoint: string): Promise<Record<string, unknown> | Record<string, unknown>[]> => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default async (endpoint: string): Promise<any> => {
   const response = await fetch(`https://data.police.uk/api${endpoint}`)
   return response.json()
 }
