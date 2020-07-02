1ab1a37022123f789d8cfb8dfc7000504c19c704
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2020-07-02 10:06:12 +0100

    remove unneeded file

--- src/types.d.ts
+++
@@ -1,8 +0,0 @@
-import { ICrime, IAvailability, ICategory } from './interfaces'
-
-export function availability(): IAvailability[]
-export function categories(): ICategory[]
-export function forces(force?: string): Record<string, unknown>[]
-export function lastUpdated(): Record<string, unknown>[]
-export function stopAndSearch(latitude: number, longitude: number, date?: string): Record<string, unknown>[]
-export function streetLevel(latitude: number, longitude: number, date?: string): ICrime[]
