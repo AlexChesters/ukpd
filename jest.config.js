eb5dc913ab2292d6a3a16754f6e726955a1de705
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-01-13 20:03:25 +0000

    run tests against ts code

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
-const { defaults } = require('jest-config') // eslint-disable-line @typescript-eslint/no-var-requires
-
 module.exports = {
-  testPathIgnorePatterns: [
-    ...defaults.testPathIgnorePatterns,
-    '\.ts$' // eslint-disable-line no-useless-escape
-  ]
+  testPathIgnorePatterns: ['build/'],
+  transform: {
+    '^.+.[jt]s$': 'ts-jest'
+  }
 }
