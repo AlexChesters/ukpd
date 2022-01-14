82949fc18e76200fbbd8ae9bc3f01bc15d1ea157
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-01-14 23:14:22 +0000

    fix broken examples

--- examples/stop-and-search.js
+++ examples/stop-and-search.js
@@ -3,7 +3,7 @@
 // obtain a reference to the module
 // when using the module in your project, this line would be
 // const traffic = require('ukpd')
-const UKPD = require('../build/src')
+const UKPD = require('../build')
 
 async function main () {
   const results = await UKPD.stopAndSearch('cheshire')
